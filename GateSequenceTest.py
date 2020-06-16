92e36a761d9702494d94b22f8b67fb10f1f99fbe
Matthew Schumaker <mschumaker@giantspiral.com>
2020-06-16 11:02:23 -0400

    Cleaned up the code

--- GateSequenceTest.py
+++ GateSequenceTest.py
@@ -10,10 +10,12 @@ bus = smbus.SMBus(DEVICE_BUS)
 
 okRiders = sa.WaveObject.from_wave_file('OkRidersRandomStart.wav')
 ridersReady = sa.WaveObject.from_wave_file('RidersReadyWatchTheGate.wav')
+shortTone = sa.WaveObject.from_wave_file('632_60ms.wav')
+longTone = sa.WaveObject.from_wave_file('632_2250ms.wav')
 
-wave_short = sa.WaveObject.from_wave_file('632_60ms.wav')
-wave_long = sa.WaveObject.from_wave_file('632_2250ms.wav')
-
+def playAudioBlocking( wave ):
+    play_obj = wave.play()
+    play_obj.wait_done()
 
 def turnOnRelay( relayNumber ):
     bus.write_byte_data(DEVICE_ADDR, relayNumber, 0xFF)
@@ -23,26 +25,21 @@ def turnOffRelay( relayNumber ):
 
 while True:
     
-    play_obj = okRiders.play()
-    play_obj.wait_done()
-    
+    playAudioBlocking(okRiders)
     t.sleep(1.8)
     
-    play_obj = ridersReady.play()
-    play_obj.wait_done()
-    
+    playAudioBlocking(ridersReady)
     t.sleep(1.5)
     
     try:
         for i in range(1,4):
             turnOnRelay(i)
-            play_obj = wave_short.play()
-            play_obj.wait_done()
+            playAudioBlocking(shortTone)
             t.sleep(0.060)
             
         turnOnRelay(4)
-        play_obj = wave_long.play()
-        play_obj.wait_done()
+        playAudioBlocking(longTone)
+        
         for i in range(1,5):
             turnOffRelay(i)
         t.sleep(5.00)    
